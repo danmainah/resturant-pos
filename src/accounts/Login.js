045944bf3ee8ec4e6e5d3b0e230478ebcb0cccc0
Danmainah <danielmainawaweru@gmail.com>
2022-06-03 22:05:15 +0300

    Add login page and its style

--- src/accounts/Login.js
+++ src/accounts/Login.js
@@ -1,7 +1,28 @@
 import React from 'react'
+import './css/Login.css'
 
 export const Login = () => {
   return (
-    <div>Login</div>
+    <form>
+        <h1>Employer Log in</h1>
+        <div className="inset">
+        <p>
+            <label for="email">EMAIL ADDRESS</label>
+            <input type="text" name="email" id="email" />
+        </p>
+        <p>
+            <label for="password">PASSWORD</label>
+            <input type="password" name="password" id="password" />
+        </p>
+     <p>
+      <input type="checkbox" name="remember" id="remember" />
+      <label for="remember">Remember me for 14 days</label>
+    </p>
+    </div>
+    <p className="p-container">
+      <span>Forgot password ?</span>
+      <input type="submit" name="go" id="go" value="Log in" />
+    </p>
+  </form>
   )
 }
