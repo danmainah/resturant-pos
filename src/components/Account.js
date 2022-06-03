28db35e9ca02cfd5d4a6b9e4782cbd0af0e37234
Danmainah <danielmainawaweru@gmail.com>
2022-06-03 18:48:24 +0300

    Add accounts directories

---
+++ src/components/Account.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Admin } from '../accounts/Admin'
+import { Login } from '../accounts/Login'
+import { Signup } from '../accounts/Signup'
+
+export const Account = () => {
+  return (
+    <>
+    <Admin />
+    <Login />
+    <Signup />
+    </>
+  )
+}
